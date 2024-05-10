201278f737b50291a5b01d185bc3e7f042a45187
LadipoSanni <101804382+LadipoSanni@users.noreply.github.com>
2024-05-10 16:35:28 +0100

    Add files via upload

---
+++ mian.tf
@@ -0,0 +1,10 @@
+#create vpc
+resource "aws_vpc" "vpc"{
+    cidr_block   =  "var.vpc_cidr"
+    instance_tenancy = "default"
+    enable-dns-hostname = true
+    enable-dns-support = true
+    tags  = {
+        Name = "${var.project_name}-vpc"
+
+    }
